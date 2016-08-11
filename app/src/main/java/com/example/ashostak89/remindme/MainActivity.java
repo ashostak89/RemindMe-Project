b3c7750b5a3b0c1e9983bd872c4ee907b22eb0f3
ashostak89 <ashostak89@gmail.com>
2016-08-11 21:56:58 +0300

    drawer...

--- app/src/main/java/com/example/ashostak89/remindme/MainActivity.java
+++ app/src/main/java/com/example/ashostak89/remindme/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.ashostak89.remindme;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity{
 Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
+       setTheme(R.style.AppDefult);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initToolbar();
